9e2e70cf142c387738988847d9e2a3de694208fa
Allex113 <ya.nokia113@gmail.com>
2025-07-09 22:05:43 +0300

    Init

---
+++ point.js
@@ -0,0 +1,22 @@
+export class Point{
+    constructor (posX, posY) {
+        this.posX = posX;
+        this.posY = posY;
+
+        this.upperX = posX - 16;
+        this.lowerX = posX + 16;
+
+        this.upperY = posY - 16;
+        this.lowerY = posY + 16;
+    }
+
+    posX = 0;
+    posY = 0;
+
+    upperX = 0;
+    upperY = 0;
+
+    lowerX = 0;
+    lowerY = 0;
+
+}
---
+++ scr.js
@@ -0,0 +1,87 @@
+//@ts-check
+import { Point } from "./point";
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const imgSize = 64;
+const ImgPointTool = new Image();
+ImgPointTool.src = "Sprites/PointTool.png";
+const ImgConnectTool = new Image();
+ImgConnectTool.src = "Sprites/ConnectTool.png";
+
+ImgConnectTool.onload = () => {
+    ctx.drawImage(ImgConnectTool, 64, 0, imgSize, imgSize);
+    ctx.strokeRect(64, 0, imgSize, imgSize);
+}
+ImgPointTool.onload = () => {
+    ctx.drawImage(ImgPointTool, 0, 0, imgSize, imgSize);
+    ctx.strokeRect(0, 0, imgSize, imgSize);
+}
+
+var allPoints = [];
+
+/**
+ * @param {MouseEvent} e
+*/
+var PointTool = function(e){
+    var radius = e.offsetX - (imgSize / 4);
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 360 );
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+
+    var point = new Point(x,y);
+
+}
+
+/**
+ * @param {MouseEvent} e
+*/
+var ConncectTool = function(e){
+    console.log(allPoints);
+};
+
+var CurrentTool = PointTool;
+
+function changeTool(toolNumber){
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, imgSize, imgSize);
+    ctx.strokeRect(64, 0, imgSize, imgSize);
+
+    CurrentTool = toolNumber == 0 ? PointTool : ConncectTool;
+
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(imgSize * toolNumber, 0, imgSize, imgSize);
+
+    ctx.strokeStyle = "black";
+}
+
+
+
+/**
+ * @param {MouseEvent} e
+*/
+function documentOnClick(e){
+    var toolNumber = ~~(e.offsetX / 64);
+
+    if(e.offsetY <= imgSize){
+        changeTool(toolNumber);
+        return;
+    }
+    
+    CurrentTool(e);
+
+    
+    //console.log(CurrentTool);
+}
+
+
+document.addEventListener("click", e => {
+    e.preventDefault();
+    documentOnClick(e);
+})
+
