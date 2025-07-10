faa2198b1975a85cc5abf9c9fdc286b27bd1ef1e
Allex113 <ya.nokia113@gmail.com>
2025-07-10 20:16:36 +0300

    Better overlap check

--- Utils/utils.js
+++ Utils/utils.js
@@ -4,10 +4,10 @@
  */
 export function overlapCheck(x, y, allPoints){
     var finds = allPoints.find(item => 
-        x >= item.upperX && 
-        x <= item.lowerX && 
-        y >= item.upperY && 
-        y <= item.lowerY
+        x >= item.upperX - 16 && 
+        x <= item.lowerX + 16 && 
+        y >= item.upperY - 16 && 
+        y <= item.lowerY + 16
         );
     
     return !!finds;
